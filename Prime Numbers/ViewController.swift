1c40924b457c32b34d1bdd80c70d2bf0c9f57816
RootA <amwathi@fluidtechglobal.com>
2018-06-18 19:59:27 +0300

    Initial Commit

---
+++ Prime Numbers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prime Numbers
+//
+//  Created by Antony Mwathi on 18/06/2018.
+//  Copyright © 2018 Antony Mwathi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
