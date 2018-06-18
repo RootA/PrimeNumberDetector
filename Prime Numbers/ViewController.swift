cc2be3655e389a41546f0b16b373ae59cd1d214d
RootA <amwathi@fluidtechglobal.com>
2018-06-18 20:35:03 +0300

    first commit

--- Prime Numbers/ViewController.swift
+++ Prime Numbers/ViewController.swift
@@ -10,6 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var enterNumber: UITextField!
+    
+    @IBAction func getPrime(_ sender: Any) {
+        if let userEnteredString = enterNumber.text {
+            let enteredNumber = Int(userEnteredString)
+            
+            if let number = enteredNumber {
+                var isPrime = true
+                if number == 1 {
+                    isPrime = false
+                }
+                
+                var i = 2
+                while i < number {
+                    if number % i == 0 {
+                        isPrime = false
+                    }
+                    i += 1
+                }
+                if isPrime {
+                    responseLabel.text = "The number is Prime"
+                }else {
+                    responseLabel.text = "The number is not a prime"
+                }
+            } else {
+                responseLabel.text = "Please enter a positive whole number"
+            }
+        }
+    }
+    @IBOutlet weak var responseLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
